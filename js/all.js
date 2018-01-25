bd94a513e2e913a2b639c47827f562090a9bbe6c
Dasha <etterwin.yll@gmail.com>
2018-01-25 10:44:36 +0300

    udate

--- js/all.js
+++ js/all.js
@@ -1,21 +1,25 @@
+//create local time
+var date = new Date();
+var minute = date.getMinutes();
+if (minute < 10) {
+    minute = '0'+ minute;
+}
+
+//companion's phrases
 function dialog() {
 
-    var date = new Date();
-    var minute = date.getMinutes();
+    //take parent's container
+    var container = document.getElementById('messages');
 
-    if (minute < 10) {
-        minute = '0'+ minute;
-    }
-
-    var array = ['<p class="cloud-text">私のおっぱい好き？</p>\n' + '<span class="cloud-time">'+date.getHours()+':'+minute+'</span>\n',
-        '<img src="assets/pictures/png/test.png">' + '<span class="cloud-time">'+date.getHours()+':'+minute+'</span>\n',
-        '<p class="cloud-text">トロント付近に住んでいるなら、ぜひ会いに来て！</p>' + '<span class="cloud-time">'+date.getHours()+':'+minute+'</span>\n',
-        '<p class="cloud-text"><a href="http://adbomb.pro/click.php?lp=1">私のプロフィールを見てみて</a></p>\n' + '<span class="cloud-time">'+date.getHours()+':'+minute+'</span>\n',
-        '<p class="cloud-text">連絡先を登録して、お近づきになりましょう</p>\n' + '<span class="cloud-time">'+date.getHours()+':'+minute+'</span>\n',
-        '<p class="cloud-text">アナルプレーもできるわ。どうやって私をイカせてくれるのか教えて？</p>\n' + '<span class="cloud-time">'+date.getHours()+':'+minute+'</span>\n',
-        '<p class="cloud-text">私の身体の上で、男性が満足するのを見るのが本当に好きなの…</p>\n' + '<span class="cloud-time">'+date.getHours()+':'+minute+'</span>\n',
-        '<p class="cloud-text">セックスしたくてたまらない。写真を撮って <a href="http://adbomb.pro/click.php?lp=1">に載せるから見てみて！</a></p>\n' + '<span class="cloud-time">'+date.getHours()+':'+minute+'</span>\n',
-        '<p class="cloud-text">すぐまたあとでチャットしましょうxx</p>\n' + '<span class="cloud-time">'+date.getHours()+':'+minute+'</span>\n']
+    var array = ['<div class="avatar"></div>'+'<p class="cloud-text">私のおっぱい好き？</p>\n' + '<span class="cloud-time">'+date.getHours()+':'+minute+'</span>\n',
+        '<div class="avatar"></div>'+'<img src="assets/pictures/png/girl.png" class="girl" />' + '<span class="cloud-time">'+date.getHours()+':'+minute+'</span>\n',
+        '<div class="avatar"></div>'+'<p class="cloud-text">トロント付近に住んでいるなら、ぜひ会いに来て！</p>' + '<span class="cloud-time">'+date.getHours()+':'+minute+'</span>\n',
+        '<div class="avatar"></div>'+'<p class="cloud-text"><a href="http://adbomb.pro/click.php?lp=1">私のプロフィールを見てみて</a></p>\n' + '<span class="cloud-time">'+date.getHours()+':'+minute+'</span>\n',
+        '<div class="avatar"></div>'+'<p class="cloud-text">連絡先を登録して、お近づきになりましょう</p>\n' + '<span class="cloud-time">'+date.getHours()+':'+minute+'</span>\n',
+        '<div class="avatar"></div>'+'<p class="cloud-text">アナルプレーもできるわ。どうやって私をイカせてくれるのか教えて？</p>\n' + '<span class="cloud-time">'+date.getHours()+':'+minute+'</span>\n',
+        '<div class="avatar"></div>'+'<p class="cloud-text">私の身体の上で、男性が満足するのを見るのが本当に好きなの…</p>\n' + '<span class="cloud-time">'+date.getHours()+':'+minute+'</span>\n',
+        '<div class="avatar"></div>'+'<p class="cloud-text">セックスしたくてたまらない。写真を撮って <a href="http://adbomb.pro/click.php?lp=1">に載せるから見てみて！</a></p>\n' + '<span class="cloud-time">'+date.getHours()+':'+minute+'</span>\n',
+        '<div class="avatar"></div>'+'<p class="cloud-text">すぐまたあとでチャットしましょうxx</p>\n' + '<span class="cloud-time">'+date.getHours()+':'+minute+'</span>\n'];
 
 
     var article_0 = document.createElement('article');
@@ -24,8 +28,26 @@ function dialog() {
     var article_1 = document.createElement('article');
     article_1.className = "cloud";
 
+    var article_2 = document.createElement('article');
+    article_2.className = "cloud";
 
-    var container = document.getElementById('messages');
+    var article_3 = document.createElement('article');
+    article_3.className = "cloud";
+
+    var article_4 = document.createElement('article');
+    article_4.className = "cloud";
+
+    var article_5 = document.createElement('article');
+    article_5.className = "cloud";
+
+    var article_6 = document.createElement('article');
+    article_6.className = "cloud";
+
+    var article_7 = document.createElement('article');
+    article_7.className = "cloud";
+
+    var article_8 = document.createElement('article');
+    article_8.className = "cloud";
 
     setTimeout(function () {
         article_0.innerHTML = array[0];
@@ -33,18 +55,61 @@ function dialog() {
         setTimeout(function () {
             article_1.innerHTML = array[1];
             container.appendChild(article_1);
+            setTimeout(function () {
+                article_2.innerHTML = array[2];
+                container.appendChild(article_2);
+                setTimeout(function () {
+                    article_3.innerHTML = array[3];
+                    container.appendChild(article_3);
+                    setTimeout(function () {
+                        article_4.innerHTML = array[4];
+                        container.appendChild(article_4);
+                        setTimeout(function () {
+                            article_5.innerHTML = array[5];
+                            container.appendChild(article_5);
+                            setTimeout(function () {
+                                article_6.innerHTML = array[6];
+                                container.appendChild(article_6);
+                                setTimeout(function () {
+                                    article_7.innerHTML = array[7];
+                                    container.appendChild(article_7);
+                                    setTimeout(function () {
+                                        article_8.innerHTML = array[8];
+                                        container.appendChild(article_8);
+                                    },2800)
+                                },5700)
+                            },5000)
+                        },5400)
+                    },3700)
+                },8000)
+            },4000)
         },2000)
     },1000)
+
 }
 
+//duration
 setTimeout(dialog, 1000);
 
-
-var counter = 0;
-
+//add user comment
 function addComment() {
 
-    counter++;
-    console.log(counter);
+    //take parent's container
+    var container = document.getElementById('messages');
 
+    var article = document.createElement('article');
+    article.className = "my-cloud";
+
+    var phrase = document.getElementById('field').value;
+    if (phrase.length > 0) {
+
+        article.innerHTML = '<div class="cloud-wrapper">\n' + '<span class="my-cloud-time">'+date.getHours()+':'+minute+'</span>\n' +
+            '<p class="my-cloud-text">'+phrase+'</p>\n' + '</div>';
+        container.appendChild(article);
+        document.getElementById('field').value = '';
+    }
+    else {
+        return false;
+    }
 }
+
